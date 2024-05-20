57afe22b6031a4d30969af80cd89e4552c03d94f
subha92sde <subha92sde@gmail.com>
2024-05-20 12:02:25 +0600

    first commit for project initialization

---
+++ src/test/java/com/e/comm/business/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e.comm.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
