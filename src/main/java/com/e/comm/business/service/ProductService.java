784eebb3f970a1b0cba43d2885b8a0c93b3a47f6
subha92sde <subha92sde@gmail.com>
2024-05-21 17:15:38 +0600

    ProductService added for establishing business logic for api response

--- src/main/java/com/e/comm/business/service/ProductService.java
+++ src/main/java/com/e/comm/business/service/ProductService.java
@@ -1,2 +1,12 @@
-package com.e.comm.business.service;public class ProductService {
+package com.e.comm.business.service;
+
+import com.e.comm.business.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
 }
