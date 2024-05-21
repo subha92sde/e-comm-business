c4937b4110f8da23e61b69317203233e5d78a110
subha92sde <subha92sde@gmail.com>
2024-05-21 17:16:12 +0600

    developed API with end point /product/get-all

--- src/main/java/com/e/comm/business/controller/ProductController.java
+++ src/main/java/com/e/comm/business/controller/ProductController.java
@@ -1,2 +1,20 @@
-package com.e.comm.business.controller;public class ProductController {
+package com.e.comm.business.controller;
+
+import com.e.comm.business.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductRepository productRepository;
+
+    @RequestMapping(value = "/product/get-all", method = RequestMethod.GET)
+    public ResponseEntity<?> getAllProducts() {
+        return new ResponseEntity<>(productRepository.findAll(), HttpStatus.OK);
+    }
 }
