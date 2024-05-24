0d5b6d1c13920e25a0aba8f1af6a61bd2a13e7f1
subha92sde <subha92sde@gmail.com>
2024-05-24 14:52:30 +0600

    BuyerService created

--- src/main/java/com/e/comm/business/service/BuyerService.java
+++ src/main/java/com/e/comm/business/service/BuyerService.java
@@ -1,2 +1,11 @@
-package com.e.comm.business.service;public class BuyerService {
+package com.e.comm.business.service;
+
+import com.e.comm.business.repository.BuyerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BuyerService {
+    private final BuyerRepository buyerRepository;
 }
