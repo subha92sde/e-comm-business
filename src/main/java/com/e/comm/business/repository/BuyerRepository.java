608de13d31115ad10cd62573dbed6a7bb09c1ec1
subha92sde <subha92sde@gmail.com>
2024-05-24 14:52:06 +0600

    BuyerRepository created

--- src/main/java/com/e/comm/business/repository/BuyerRepository.java
+++ src/main/java/com/e/comm/business/repository/BuyerRepository.java
@@ -1,2 +1,10 @@
-package com.e.comm.business.repository;public interface BuyerRepository {
+package com.e.comm.business.repository;
+
+import com.e.comm.business.model.Buyer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BuyerRepository extends JpaRepository<Buyer, Long> {
+    
 }
